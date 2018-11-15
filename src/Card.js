03f0754966626149f9778de87c0fccb5af72bdd8
Keith Bender <k.bender30@gmail.com>
2018-11-15 14:57:54 -0600

    added searchbox and robot states

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+        <div className="bg-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc">
             <img src={`https://robohash.org/${props.id}?200x200`} alt='robots'/>
             <div>
                 <h2>{ props.name }</h2>
